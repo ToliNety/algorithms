9babcd309189e14e4052547335a514116866d5f7
tolinety <tolinety@gmail.com>
2019-07-07 22:27:10 +0300

    add board & solution impl

--- 8Puzzle/src/Board.java
+++ 8Puzzle/src/Board.java
@@ -1,29 +1,127 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Board {
     private final int[][] blocks;
+    private final int length;
+    private int i0;
+    private int j0;
     private final int manhattan;
 
     public Board(int[][] blocks) {
         this.blocks = blocks;
+        this.length = blocks.length;
 
+        if (2 > length || length > 127)
+            throw new IllegalArgumentException();
+
+        int manhattan = 0;
+        for (int j = 0; j < length; j ++) {
+            for (int i = 0; i < length; i++) {
+                int num = blocks[i][j];
+                if (num != i + 1 + j * length) {
+                    int x = num % length == 0 ? length : num % length - 1;
+                    int y = num / length;
+                    manhattan += Math.abs(x - i) + Math.abs(y - j);
+                }
+                if (num == 0) {
+                    i0 = i;
+                    j0 = j;
+                }
+            }
+        }
+        this.manhattan = manhattan;
     }
 
-    public int dimension(){
-        return blocks.length;
+    public int dimension() {
+        return length;
     }
 
-    public int hamming(){
+    public int hamming() {
         return 0;
     }
 
-    public int manhattan(){
+    public int manhattan() {
         return manhattan;
     }
 
-    public boolean isGoal()                // is this board the goal board?
-    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y)        // does this board equal y?
-    public Iterable<Board> neighbors()     // all neighboring boards
-    public String toString()               // string representation of this board (in the output format specified below)
+    public boolean isGoal() {
+        return manhattan == 0;
+    }
+
+    public Board twin() {
+        return this;
+    }
+
+    public boolean equals(Object y) {
+        if (y.getClass() != this.getClass())
+            return false;
+        Board yBoard = (Board) y;
+
+        if (yBoard.length != this.length)
+            return false;
+
+        if (yBoard.manhattan != this.manhattan)
+            return false;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (this.blocks[i][j] != yBoard.blocks[i][j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Iterable<Board> neighbors() {
+        List<Board> neighbors = new LinkedList<>();
+        if (i0 > 0) {
+            int newBlocks[][] = blocks.clone();
+            int e = newBlocks[i0 - 1][j0];
+            newBlocks[i0 - 1][j0] = 0;
+            newBlocks[i0][j0] = e;
+            neighbors.add(new Board(newBlocks));
+        }
+
+        if (i0 < length - 1){
+            int newBlocks[][] = blocks.clone();
+            int e = newBlocks[i0 + 1][j0];
+            newBlocks[i0 + 1][j0] = 0;
+            newBlocks[i0][j0] = e;
+            neighbors.add(new Board(newBlocks));
+        }
+
+        if (j0 > 0){
+            int newBlocks[][] = blocks.clone();
+            int e = newBlocks[i0][j0 - 1];
+            newBlocks[i0][j0 - 1] = 0;
+            newBlocks[i0][j0] = e;
+            neighbors.add(new Board(newBlocks));
+        }
+
+        if (j0 < length - 1){
+            int newBlocks[][] = blocks.clone();
+            int e = newBlocks[i0][j0 + 1];
+            newBlocks[i0][j0 + 1] = 0;
+            newBlocks[i0][j0] = e;
+            neighbors.add(new Board(newBlocks));
+        }
+
+        return neighbors;
+    }
+
+    public String toString() {
+        String out = "";
+        for (int j = 0; j < length; j ++) {
+            for (int i = 0; i < length; i++) {
+                out += blocks[i][j];
+            }
+            out += System.lineSeparator();
+        }
+
+        return out;
+    }
 
     public static void main(String[] args) {
 
--- 8Puzzle/src/Solver.java
+++ 8Puzzle/src/Solver.java
@@ -1,11 +1,54 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    public boolean isSolvable()            // is the initial board solvable?
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+    private final List<Board> solution = new LinkedList<>();
+    private final MinPQ<Node> pq;
+    private final Node rootNode;
+
+    public Solver(Board initial) {
+        pq = new MinPQ<>(Comparator.comparingInt(Node::priority));
+
+        Node rootNode = new Node(initial, null, 0);
+        pq.insert(rootNode);
+        this.rootNode = rootNode;
+
+        boolean isFound = false;
+        while (!isFound){
+            Node nodeProcessed = pq.delMin();
+            if (nodeProcessed.getBoard().isGoal()){
+                isFound = true;
+            } else {
+                addNeighborsToGame(nodeProcessed);
+            }
+        }
+
+    }
+
+    private void addNeighborsToGame(Node nodeProcessed) {
+        Iterable<Board> neighbors = nodeProcessed.getBoard().neighbors();
+        neighbors.forEach(neighbor -> {
+            Node neighborNode = new Node(neighbor, nodeProcessed.board, nodeProcessed.move + 1);
+            pq.insert(neighborNode);
+        });
+    }
+
+    public boolean isSolvable() {
+        return true;
+    }
+
+    public int moves() {
+        return solution.size();
+    }
+
+    public Iterable<Board> solution() {
+        return solution;
+    }
 
     public static void main(String[] args) {
         // create initial board from file
@@ -27,6 +70,34 @@ public class Solver {
             StdOut.println("Minimum number of moves = " + solver.moves());
             for (Board board : solver.solution())
                 StdOut.println(board);
+        }
+    }
+
+    private static class Node {
+        private final Board board;
+        private final Board parent;
+        private final int move;
+
+        public Node(Board board, Board parent, int move) {
+            this.board = board;
+            this.parent = parent;
+            this.move = move;
+        }
+
+        public Board getBoard() {
+            return board;
+        }
+
+        public Board getParent() {
+            return parent;
+        }
+
+        public int getMove() {
+            return move;
+        }
+
+        public int priority() {
+            return move + board.manhattan();
         }
     }
 }
